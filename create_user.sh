abe20e13cf647cbe574df38d70c7220bdc4e5c27
Nikita Arkhipov <niktyper13@gmail.com>
2019-01-01 17:04:40 +0000

    add separate ssh config

--- create_user.sh
+++ create_user.sh
@@ -3,19 +3,9 @@
 user=$1
 adduser $user
 usermod -aG sudo $user
+
 cp -R /root/.ssh /home/$user/
-
-echo \
-"Host github.com
-  HostName github.com
-  User git
-  IdentityFile ~/.ssh/github_rsa
-
-Host bitbucket.com
-  HostName github.com
-  User git
-  IdentityFile ~/.ssh/bitbucket_rsa
-" > /home/$user/.ssh/config
+cp ./ssh_config /home/$user/.ssh/config
 
 chown -R $user:$user /home/$user/.ssh
 
